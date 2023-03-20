a7b9dad975d6ff6247e3487e254fc8a7400747a9
pjdijxhoorn <pjdijxhoorn@hotmail.com>
2023-03-20 09:25:56 +0100

    added 1 excercise in lesson 2

--- Les2.py
+++ Les2.py
@@ -1,4 +1,4 @@
-from les1 import les1
+
 
 def les2():
     print("")
@@ -145,4 +145,106 @@ def les2():
     for number1 in range(1, 11):
         for number2 in range(1, 11):
             print(number1, "X", number2, " = ", number1 * number2)
+
+
+
+
+    # ------------opdracht4----------
+    import random
+
+
+    """
+    We gaan hier verder met het raadspel van les 1.
+    we gaan het raadspel zo maken dat het slechts 1 berekening geeft. 
+    En dat de speler tips kan bij vragen wat de overige berekeningen zijn.
+    ook gaan we punten bij houden hoe meer tips een speler vraagt des minder punten verdient kunnen worden.
+    
+    Stap1:  maak twee variabele maximaal en mininmaal.
+            de waarde van deze variabele is een int (input()) met als tekst "Give a max number for the game: "
+            en "Give a minimum number for the game: " .
+    Stap2:  Maak nog twee varaibele randomNumber1 en randomNumber2 en vul deze met random.randint() 
+            met de minimaal en maximaal als bereik. hierdoor krijge je een getal tussen minimaal en maximaal.
+    Stap3:  maak een variabele punten en geef deze de waarde 6.
+    Stap4:  Maak een while loop met als conditie True
+    Stap5:  Als het aantal punten gelijk is aan 6 print dan een welkoms bericht 
+            en geef de eerste tip (1 van de berekeningen)
+    Stap6:  Maak een variabele userinput en geef deze de waarde van een input() met als tekst
+            "Do you want a tip fill in tip, or do you want to guess the fill in guess: "
+    Stap7:  Maak een if-elif-else statement. De if statement test voor tip de elif test voor guess. 
+            en de else statement bevat een print "incorrect input please fill in guess or tip"
+    Stap8:  in de if statement met de conditie userinput == "tip" doe je een aantal dingen. 
+            Eerst haal je een punt af van punten. vervolgens maak je een if- elif - else statement die test voor
+            het aantal punten. omdat dat minder wordt per keer dat tip gekozen wordt kan je dit gebruiken om 
+            de overige tips uit te printen. 
+            omdat we niet onder de 1 willen komen is de else statement een 1+ voor punten en een print 
+            "No tips left sorry!" 
+    Stap9:  Nu gaan we de Elif met de conditie userinput == "guess" afmaken.
+            maak hierin twee variabele aan guessNumber1 en guessNumber2 deze worden gevuld 
+            met een int waarde gegeven door de user. geef deze de tekst: What do you think number 1 is:
+            en hetzelfde voor het tweede nummer. Maak vervolgens een if -else statement die test of 
+            guessnumber1 gelijk is aan randomnumber 1 en guessnumber2 gelijk is aan randomnumber2.
+            als dat zo is dan krijg je een print met een overwinnings bericht en het punten aantal dat gescoord is.
+            maak hier ook een break.
+            anders maak je een print met "sorry that was incorrect try again or ask for another tip!"
+            
+     BONUS: Breid het spel zo uit dat je meerdere rondes kan spelen en hierbij je score steeds verder kan uitbreiden.
+     """
+
+    totalpoints = 0
+
+    while True:
+
+        maximaal = int(input("Give a max number for the game: "))
+        minimaal = int(input("Give a minimum number for the game: "))
+        randomNumber1 = random.randint(minimaal, maximaal)
+        randomNumber2 = random.randint(minimaal, maximaal)
+        punten = 6
+
+        while True:
+
+            if punten == 6:
+                print("Welcome to the calculation guessing game!")
+                vermenigvuldig = randomNumber1 * randomNumber2
+                print("Tip 3: number 1  X  number 2  = ", vermenigvuldig)
+
+
+            userinput = input("Do you want a tip fill in tip, or do you want to guess the fill in guess: ")
+            if userinput == "tip":
+                punten -= 1
+                if punten == 5:
+                    groter = randomNumber1 > randomNumber2
+                    print("Tip 2: number 1 is bigger then number 2 = ", groter)
+                elif punten == 4:
+                    gelijk = randomNumber1 == randomNumber2
+                    print("Tip 1: number 1 and number are equal = ", gelijk)
+                elif punten == 3:
+                    plus = randomNumber1 + randomNumber2
+                    print("Tip 4: number 1 + number 2 = ", plus)
+                elif punten == 2:
+                    aftrekken = randomNumber1 - randomNumber2
+                    print("Tip 5: number 1 - number 2 = ", aftrekken)
+                elif punten == 1:
+                    delen = randomNumber1 / randomNumber2
+                    print("Tip 6: number 1 / number 2 = ", delen)
+                else:
+                    punten += 1
+                    print("No tips left sorry!")
+
+
+            elif userinput == "guess":
+                guessNumber1 = int(input("What do you think number 1 is: "))
+                guessNumber2 = int(input("What do you think number 2 is: "))
+                if guessNumber1 == randomNumber1 and guessNumber2 == randomNumber2:
+                    totalpoints = totalpoints + punten
+                    print("Yeah you won!! you got", punten, "points and your total is now", totalpoints)
+                    break
+                else:
+                    print("sorry that was incorrect try again or ask for another tip!")
+            else:
+                print("incorrect input please fill in guess or tip")
+
+        anothergame = input("Do you want another game? y / n ")
+        if anothergame == "n":
+            break
+
 
